8757e52a6637270b9d6a398d53d89a67549d7301
WahhaWood <98262397+WahhaWood@users.noreply.github.com>
2026-09-22 21:32:52 +0300

    Update gpt.js

--- api/gpt.js
+++ api/gpt.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     const model = genAI.getGenerativeModel({
       model: 'gemini-2.5-flash',
       systemInstruction:
-        'Ты короткий бот для Twitch-чата. Отвечай максимально кратко, до 200 символов, без списков и оформления.'
+        'Ты короткий бот для Twitch-чата. Отвечай максимально кратко, до 350 символов, без списков и оформления.'
     });
 
     const result = await model.generateContent(prompt);
